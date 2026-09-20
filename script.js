6e7b59ae559710842a549e172a1fbefc1de91a46
Raiyan mrf <140499799+raiyanmrf@users.noreply.github.com>
2026-09-20 20:51:46 +0600

    print() -> toString()

--- script.js
+++ script.js
@@ -35,15 +35,15 @@ class LinkedList {
     currentNode.next = newNode;
   }
 
-  print() {
+  toString() {
     let str = "";
     if (this.isEmpty()) return str;
     let currentNode = this.head;
     while (currentNode.next !== null) {
-      str += `[ ${currentNode.value} ]`;
+      str += `( ${currentNode.value} ) -> `;
       currentNode = currentNode.next;
     }
-    str += `[ ${currentNode.value} ]`; // extra line for printing last node
+    str += `( ${currentNode.value} )`; // extra line for printing last node
     return str;
   }
   size() {
@@ -118,7 +118,7 @@ linkedList.append(5);
 linkedList.append(6);
 linkedList.append(7);
 linkedList.prepend(1);
-console.log(linkedList.print());
+console.log(linkedList.toString());
 console.log("size: ", linkedList.size());
 console.log("head: ", linkedList.getHeadValue());
 console.log("tail: ", linkedList.getTailValue());
